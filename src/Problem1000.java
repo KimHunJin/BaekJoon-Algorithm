4a90fe0ef9882f410b5f02eb4c09548b48b5a913
KimHunJin <ysg01129@naver.com>
2016-02-16 15:42:10 +0900

    문제 1000번 문제 1001번 문제 1002번 실패

---
+++ src/Problem1000.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by HunJin on 2016-02-16.
+ */
+public class Problem1000 {
+    public static void main(String[] args) {
+
+        int input1, input2;
+        Scanner sc = new Scanner(System.in);
+        input1 = sc.nextInt();
+        input2 = sc.nextInt();
+
+        System.out.println(input1 + input2);
+
+    }
+}
---
+++ src/Problem1002.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+/**
+ * Created by HunJin on 2016-02-16.
+ */
+public class Problem1002 {
+    public static void main(String[] args) {
+        int T,x1, y1, r1, x2, y2, r2;
+        Scanner sc = new Scanner(System.in);
+
+        T = sc.nextInt();
+
+        for(int i=0;i<T;i++) {
+            x1 = sc.nextInt();
+            y1 = sc.nextInt();
+            r1 = sc.nextInt();
+            x2 = sc.nextInt();
+            y2 = sc.nextInt();
+            r2 = sc.nextInt();
+
+            double ab = Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
+            double ac = r1;
+            double bc = r2;
+
+            double min1, min2;
+
+            double max = ab;
+            if(max<ac) {
+                min1 = max;
+                max = ac;
+            } else {
+                min1 = ac;
+            }
+            if(max<bc) {
+                min2 = max;
+                max = bc;
+            } else {
+                min2 = bc;
+            }
+
+            if(max > min1+min2) {
+                System.out.println(0);
+            } else if(Math.abs((ac+bc))==ab) {
+                System.out.println(1);
+            } else if(x1==x2&&y1==y2&&r1==r2) {
+                System.out.println(-1);
+            } else{
+                System.out.println(2);
+            }
+        }
+    }
+}
