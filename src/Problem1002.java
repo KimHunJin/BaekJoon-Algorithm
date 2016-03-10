6adc8438bfecc3e76ac29d24299d45cbf83ae31a
KimHunJin <ysg01129@naver.com>
2016-03-10 16:45:17 +0900

    success 1002

--- src/Problem1002.java
+++ src/Problem1002.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 /**
  * Created by HunJin on 2016-02-16.
  *
- * fail
  */
 public class Problem1002 {
-    public static void main(String[] args) {
-        int T,x1, y1, r1, x2, y2, r2;
-        Scanner sc = new Scanner(System.in);
 
+    static int T,x1, y1, r1, x2, y2, r2;
+    static double d, abs, r1r2;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         T = sc.nextInt();
 
         for(int i=0;i<T;i++) {
+
             x1 = sc.nextInt();
             y1 = sc.nextInt();
             r1 = sc.nextInt();
@@ -20,35 +21,26 @@ public class Problem1002 {
             y2 = sc.nextInt();
             r2 = sc.nextInt();
 
-            double ab = Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
-            double ac = r1;
-            double bc = r2;
+            d = Math.sqrt((Math.pow((x1-x2),2) + Math.pow((y1-y2),2)));
+            abs = Math.abs(r2-r1);
+            r1r2 = r1 + r2;
 
-            double min1, min2;
-
-            double max = ab;
-            if(max<ac) {
-                min1 = max;
-                max = ac;
-            } else {
-                min1 = ac;
-            }
-            if(max<bc) {
-                min2 = max;
-                max = bc;
-            } else {
-                min2 = bc;
-            }
-
-            if(max > min1+min2) {
-                System.out.println(0);
-            } else if(Math.abs((ac+bc))==ab) {
-                System.out.println(1);
-            } else if(x1==x2&&y1==y2&&r1==r2) {
+            if(x1==x2 && y1==y2 && r1 == r2) {
                 System.out.println(-1);
-            } else{
-                System.out.println(2);
+            } else if(abs > d) {
+                System.out.println(0);
+            } else if(d == abs) {
+                System.out.println(1);
+            } else {
+                if(r1r2 > d) {
+                    System.out.println(2);
+                } else if(r1r2 == d) {
+                    System.out.println(1);
+                } else if(r1r2 <d) {
+                    System.out.println(0);
+                }
             }
+
         }
     }
 }
